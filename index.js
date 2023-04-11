61adbc28dfa948015ac25b4bb29a3017d553689f
LillyVulp <lillyvulp@gmail.com>
2023-04-10 20:25:34 -0700

    Added config data, gitignore, and login code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("npm start is working...")
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, c => {
+    console.log("Logged in successfully");
+});
+
+client.login(token);
